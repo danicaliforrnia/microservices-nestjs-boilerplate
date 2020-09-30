04bf7b21b3f0f585dab80c9d05d9d69803f489be
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-09-29 20:35:02 -0400

    feat: @nestjs/microservices package installed to transform the application to a microservices.
    -Instead of HTTP methods, cmd attribute send via TCP transport layer is used to route to the correct method.

--- users/src/users/users.controller.ts
+++ users/src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {UsersService} from "./users.service";
+import {MessagePattern} from "@nestjs/microservices";
 
 @Controller('users')
 export class UsersController {
@@ -7,27 +8,29 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {
     }
 
-    @Get()
+    @MessagePattern({ cmd: "find_all_users" })
     findAll(): any[] {
         return this.usersService.findAll();
     }
 
-    @Get('username/:username')
+    // @Get('username/:username')
+    @MessagePattern({ cmd: "find_user_by_username" })
     findByUsername(@Param() username: string): any {
         return this.usersService.findByUsername(username);
     }
 
-    @Post()
+    @MessagePattern({ cmd: "create_user" })
     create(@Body() user: any): any {
         return this.usersService.create(user);
     }
 
-    @Put()
+    @MessagePattern({ cmd: "update_user" })
     update(@Body() user: any): any {
         return this.usersService.update(user);
     }
 
-    @Delete(':usrId')
+    // @Delete(':usrId')
+    @MessagePattern({ cmd: "delete_user" })
     delete(@Param() usrId: number) {
         return this.usersService.delete(usrId);
     }
