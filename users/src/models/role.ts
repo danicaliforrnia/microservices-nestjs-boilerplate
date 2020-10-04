d7df363ddb227ca2df76f54a72fd2428d7bfe5cc
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-10-04 14:37:13 -0400

    feat: user and role entities with their respective annotations.

--- users/src/models/role.ts
+++ users/src/models/role.ts
@@ -1,3 +1,47 @@
-export class Role {
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
+@Entity({
+    name: 'role',
+    schema: 'users'
+})
+export class Role {
+    @PrimaryGeneratedColumn({
+        name: 'rol_id'
+    })
+    id: number;
+
+    @Column({
+        name: 'rol_code',
+        length: 15,
+        type: 'varchar'
+    })
+    code: string;
+
+    @Column({
+        name: 'rol_name',
+        length: 50,
+        type: 'varchar'
+    })
+    name: string;
+
+    @Column({
+        name: 'rol_active',
+        type: 'boolean'
+    })
+    isActive: boolean;
+
+    @CreateDateColumn({
+        name: 'rol_created_at'
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        name: 'rol_updated_at'
+    })
+    updatedAt: Date;
+
+    @DeleteDateColumn({
+        name: 'rol_deleted_at'
+    })
+    deletedAt: Date;
 }
--- users/src/models/user.ts
+++ users/src/models/user.ts
@@ -1,3 +1,79 @@
-export class User {
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity, JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {Role} from "./role";
 
+@Entity({
+    name: 'user',
+    schema: 'users'
+})
+export class User {
+    @PrimaryGeneratedColumn({
+        name: 'usr_id'
+    })
+    id: number;
+
+    @Column({
+        name: 'usr_username',
+        length: 50,
+        type: 'varchar'
+    })
+    username: string;
+
+    @Column({
+        name: 'usr_first_name',
+        length: 50,
+        type: 'varchar'
+    })
+    firstName: string;
+
+    @Column({
+        name: 'usr_last_name',
+        length: 50,
+        type: 'varchar'
+    })
+    lastName: string;
+
+    @Column({
+        name: 'usr_password',
+        length: 60,
+        type: 'varchar'
+    })
+    password: string;
+
+    @ManyToOne(() => Role, {
+        lazy: true,
+        nullable: false
+    })
+    @JoinColumn({
+        name: 'rol_id'
+    })
+    role: Role;
+
+    @Column({
+        name: 'usr_active',
+        type: 'boolean'
+    })
+    isActive: boolean;
+
+    @CreateDateColumn({
+        name: 'usr_created_at'
+    })
+    createdAt: Date;
+
+    @UpdateDateColumn({
+        name: 'usr_updated_at'
+    })
+    updatedAt: Date;
+
+    @DeleteDateColumn({
+        name: 'usr_deleted_at'
+    })
+    deletedAt: Date;
 }
