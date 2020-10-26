36b66bd7e2ddfa067b2abc6be3c522e4d5a3032d
Daniel Stefanelli <daniel.stefanelli.h@gmail.com>
2020-10-26 08:18:20 -0400

    refactor: each services in under services directory.

--- gateway/src/services/users-service/users/users.controller.ts
+++ gateway/src/services/users-service/users/users.controller.ts
@@ -1,7 +1,7 @@
 import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {UsersService} from './users.service';
 
-@Controller('users')
+@Controller('users-api/v1/users')
 export class UsersController {
 
     constructor(private readonly usersService: UsersService) {
